914b013cb7c0fdd2ddff7d95bcfc730f859c03ed
Josh Rickmar <jrick@companyzero.com>
2018-03-01 09:17:02 -0500

    Require atomic swap contracts to specify the secret size.
    This size is checked during audit time to ensure that the secret is of
    an expected size and will not be larger than usable by one of the
    networks.  This prevents a fraud attack where an attacker is able to
    redeem a contract using a secret that is larger than the other network
    is capable of using.

--- cmd/dcratomicswap/main.go
+++ cmd/dcratomicswap/main.go
@@ -945,6 +945,9 @@ func (cmd *auditContractCmd) runOfflineCommand() error {
 	if pushes == nil {
 		return errors.New("contract is not an atomic swap script recognized by this tool")
 	}
+	if pushes.SecretSize != secretSize {
+		return fmt.Errorf("contract specifies strange secret size %v", pushes.SecretSize)
+	}
 
 	contractAddr, err := dcrutil.NewAddressScriptHash(cmd.contract, chainParams)
 	if err != nil {
@@ -1000,6 +1003,13 @@ func atomicSwapContract(pkhMe, pkhThem *[ripemd160.Size]byte, locktime int64, se
 
 	b.AddOp(txscript.OP_IF) // Normal redeem path
 	{
+		// Require initiator's secret to be a known length that the redeeming
+		// party can audit.  This is used to prevent fraud attacks between two
+		// currencies that have different maximum data sizes.
+		b.AddOp(txscript.OP_SIZE)
+		b.AddInt64(secretSize)
+		b.AddOp(txscript.OP_EQUALVERIFY)
+
 		// Require initiator's secret to be known to redeem the output.
 		b.AddOp(txscript.OP_SHA256)
 		b.AddData(secretHash)
