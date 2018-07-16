3043d6be3cb0f1ee6addd5085cee440833a0b449
Glen De Cauwsemaecker <contact@glendc.com>
2018-07-16 15:31:37 +0200

    include binary contract code in generated Go code
    this allows the deployment (through Go) of the smart contract

--- cmd/ethatomicswap/contract/generate.go
+++ cmd/ethatomicswap/contract/generate.go
@@ -6,7 +6,7 @@ package contract
 //   cd $GOPATH/src/github.com/ethereum/go-ethereum/
 //   make
 //   make devtools
-//
 
 //go:generate sh -c "solc --abi src/contracts/AtomicSwap.sol | awk '/JSON ABI/{x=1;next}x' > AtomicSwap.abi"
-//go:generate abigen --abi=AtomicSwap.abi --pkg=contract --out=atomicswap.go
+//go:generate sh -c "solc --bin src/contracts/AtomicSwap.sol | awk '/Binary:/{x=1;next}x' > AtomicSwap.bin"
+//go:generate abigen --bin=AtomicSwap.bin --abi=AtomicSwap.abi --pkg=contract --out=atomicswap.go
--- cmd/ethatomicswap/contract/src/truffle.js
+++ cmd/ethatomicswap/contract/src/truffle.js
@@ -20,6 +20,11 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
     }
   }
 };
