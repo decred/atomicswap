fd6cfb25b0eb534e9ed3a45b35390b5a15a498e8
Glen De Cauwsemaecker <contact@glendc.com>
2018-07-20 11:37:46 +0200

    fix truffle.js as to ensure truffle test works again

--- cmd/ethatomicswap/contract/src/truffle.js
+++ cmd/ethatomicswap/contract/src/truffle.js
@@ -15,16 +15,4 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
-    },
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
-    }
-  }
 };
